a391c04464d73722050192e8af7566d6db974a67
kushaggarwal <kush.aggarwal110085@gmail.com>
2021-07-13 16:56:11 +0530

    Desktop Layout

---
+++ script.js
@@ -0,0 +1,26 @@
+function vh(v) {
+  var h = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  );
+  return (v * h) / 100;
+}
+
+(function ($) {
+  $(document).ready(function () {
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+      $(window).scroll(function () {
+        // set distance user needs to scroll before we start fadeIn
+        if ($(this).scrollTop() > vh(90)) {
+          $(".navbar").show();
+        } else {
+          $(".navbar").hide();
+        }
+      });
+    });
+  });
+})(jQuery);
